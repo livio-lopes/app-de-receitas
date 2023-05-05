99cd8c8f0ff615462be1293ec5d2d5c07a47718d
Lívio Lopes <antonio.lvio760@gmail.com>
2023-05-05 17:20:13 -0300

    Criando componentes e criando rotas

---
+++ src/components/Routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from '../pages/Login';
+import Recipes from '../pages/Recipes';
+import Profile from '../pages/Profile';
+import DoneRecipes from '../pages/DoneRecipes';
+import FavoriteRecipes from '../pages/FavoriteRecipes';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route exact path="/meals" component={ Recipes } />
+      <Route exact path="/drinks" component={ Recipes } />
+      <Route exact path="/profile" component={ Profile } />
+      <Route exact path="/profile" component={ Profile } />
+      <Route exact path="/done-recipes" component={ DoneRecipes } />
+      <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
+    </Switch>
+  );
+}
