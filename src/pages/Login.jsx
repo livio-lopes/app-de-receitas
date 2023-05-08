549c28d470c5037432ef8f005d929c8f409dede2
Lívio Lopes <antonio.lvio760@gmail.com>
2023-05-08 14:51:27 -0300

    Tela de Login
    Corrigindo rota, criando inputs para email e senha, e botão para entrar

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Login() {
+  const [user, setUser] = useState({ email: '', password: '' });
   return (
-    <div>Login</div>
+
+    <div>
+      <label htmlFor="email">
+        <input
+          type="email"
+          name="email"
+          value={ user.email }
+          data-testid="email-input"
+          placeholder="seu@email.com"
+          onChange={ ({ target: { value } }) => setUser({ ...user, email: value }) }
+        />
+      </label>
+      <label htmlFor="password">
+        <input
+          type="password"
+          name="password"
+          value={ user.password }
+          placeholder="Senha"
+          data-testid="password-input"
+          onChange={ ({ target: { value } }) => setUser({ ...user, password: value }) }
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+      >
+        Enter
+
+      </button>
+    </div>
   );
 }
