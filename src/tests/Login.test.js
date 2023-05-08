c6317d47beb34c331b9d752b08427f6909e00232
Lívio Lopes <antonio.lvio760@gmail.com>
2023-05-08 15:54:59 -0300

    Tela de Login
    implementando testes

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,3 +1,39 @@
-describe('Testando tela de Login', () => {
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import rendeWithRouter from './helpers/renderWithRouter';
+import App from '../App';
 
+const EMAIL_INPUT = 'email-input';
+const PASSWORD_INPUT = 'password-input';
+
+describe('Testando tela de Login', () => {
+  it('Verifica se existe um input email, um input para senha e um botão "Enter" desabilitado', () => {
+    rendeWithRouter(<App />);
+    const inputEmail = screen.getByTestId(EMAIL_INPUT);
+    const inputPassword = screen.getByTestId(PASSWORD_INPUT);
+    const btnEnter = screen.getByRole('button', { name: 'Enter' });
+    expect(inputEmail).toBeInTheDocument();
+    expect(inputPassword).toBeInTheDocument();
+    expect(btnEnter).toBeInTheDocument();
+  });
+  it('Verifica se botão é habilitado ao preencher email e senha validos', () => {
+    rendeWithRouter(<App />);
+    const inputEmail = screen.getByTestId(EMAIL_INPUT);
+    const inputPassword = screen.getByTestId(PASSWORD_INPUT);
+    const btnEnter = screen.getByRole('button', { name: 'Enter' });
+    userEvent.type(inputEmail, 'bode@behhh.com');
+    userEvent.type(inputPassword, '1234567');
+    expect(btnEnter).toBeEnabled();
+  });
+  it('Verifica se ao clicar no botão a tela é redirecionada para "/meals"', () => {
+    const { history } = rendeWithRouter(<App />);
+    const inputEmail = screen.getByTestId(EMAIL_INPUT);
+    const inputPassword = screen.getByTestId(PASSWORD_INPUT);
+    const btnEnter = screen.getByRole('button', { name: 'Enter' });
+    userEvent.type(inputEmail, 'bode@behhh.com');
+    userEvent.type(inputPassword, '1234567');
+    userEvent.click(btnEnter);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/meals');
+  });
 });
--- src/tests/helpers/renderWithRouter.js
+++ src/tests/helpers/renderWithRouter.js
@@ -1,3 +1,15 @@
+import { createMemoryHistory } from 'history';
+import { render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+
+function withRouter(component, history) {
+  return (
+    <Router history={ history }>
+      { component }
+    </Router>
+  );
+}
+
 export default function renderWithRouter(
   component,
   {
