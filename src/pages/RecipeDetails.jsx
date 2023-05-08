77cb2373e3dd808b9385908d9cdb4412c40362f4
AlissonSeraphim <projetoseraphim@gmail.com>
2023-05-08 15:52:14 -0300

    Requisito 24 ok, requisição na API

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default function RecipeDetails() {
-  return (
-    <div>RecipeDetails</div>
+  const searchApiDrinks = async () => {
+    const idDrink = 178319;
+    const DRINKSURL = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idDrink}`;
+    try {
+      // Bebidas
+      const resultsDrinks = await fetch(DRINKSURL);
+      const dataDrinks = await resultsDrinks.json();
+      console.log(dataDrinks);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    <h1> teste </h1>
+  const searchApiMeal = async () => {
+    const idMeal = 52771;
+    const MEALURL = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+    try {
+      // Comidas
+      const resultsFoods = await fetch(MEALURL);
+      const dataFoods = await resultsFoods.json();
+      console.log(dataFoods);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    searchApiDrinks();
+    searchApiMeal();
+  }, []);
+
+  return (
+
+    <div>
+      <h1>Tela de Detalhes Receitas</h1>
+      <button
+        onClick={ searchApiMeal }
+      >
+        trigger test comida
+      </button>
+      <button
+        onClick={ searchApiDrinks }
+      >
+        trigger test bebidas
+      </button>
+    </div>
 
   );
 }
