b7f77936ab2ac84f860b30fc99b8b9bc33c8b7d7
Rejane Lima <rejane.lima87@gmail.com>
2023-05-09 16:27:02 -0300

    Adiciona Requisito 9 com teste acima de 40%

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header() {
+  const [searchBar, setSearchBar] = useState(false);
+
   const history = useHistory();
   const { location, push } = history;
   const { pathname } = location;
-  const arrayPath = pathname.split('');
-  const title = arrayPath.splice(1, arrayPath.length);
+  const handleTitle = () => {
+    const arrayPath = pathname.split('');
+    const title = arrayPath.splice(1, arrayPath.length);
+    return title;
+  };
+
+  const handleSearchBar = () => {
+    if (searchBar === true) {
+      setSearchBar(false);
+    } else {
+      setSearchBar(true);
+    }
+  };
 
   return (
     <>
@@ -25,20 +38,32 @@ function Header() {
       </button>
       {
         (pathname === '/meals' || pathname === '/drinks')
-          && (
-            <button
-              type="submit"
-              data-testid="search-top-btn"
-              onClick={ () => console.log('a') }
-            >
-              <img
-                src={ searchIcon }
-                alt="pesquisa"
-              />
-            </button>
-          )
+        && (
+          <button
+            type="button"
+            onClick={ handleSearchBar }
+            data-testid="search-top-btn"
+          >
+            <img
+              src={ searchIcon }
+              alt="pesquisa"
+            />
+          </button>
+        )
       }
-      <h1 data-testid="page-title">{ title }</h1>
+      <h1 data-testid="page-title">{handleTitle()}</h1>
+      {
+        (searchBar)
+        && (
+          <input
+            // name="searchInput"
+            // value="searchInput"
+            data-testid="search-input"
+            placeholder="Pesquise aqui"
+            // onChange={ () => console.log('b') }
+          />
+        )
+      }
     </>
   );
 }
--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,13 +1,13 @@
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+// import { useHistory } from 'react-router-dom/cjs/react-router-dom';
+import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
 import rendeWithRouter from './helpers/renderWithRouter';
 import App from '../App';
 
 describe('Testando componente Header', () => {
   it('Verifica se todos os componentes estão na tela', () => {
     rendeWithRouter(<App />, { initialEntries: ['/done-recipes'] });
-    // const { history } =
-    // const { pathname } = history.location;
     const profileIcon = screen.getByRole('img', { name: /perfil/i });
     expect(profileIcon).toBeInTheDocument();
     const titleDoneRecipes = screen.getByRole('heading', { name: /done/i });
@@ -15,11 +15,28 @@ describe('Testando componente Header', () => {
     const btnProfile = screen.getByTestId('profile-top-btn');
     expect(btnProfile).toBeInTheDocument();
     userEvent.click(btnProfile);
-    /* console.log(pathname);
-    const title = screen.getByRole('heading', { name: /profile/i, level: 1 });
+  });
+  it('Testa se o botão da barra de pesquisa são renderizados', () => {
+    rendeWithRouter(<App />, { initialEntries: ['/meals'] });
+    const btnSearch = screen.getByRole('button', { name: /search/i });
+    expect(btnSearch).toBeInTheDocument();
+    userEvent.click(btnSearch);
+    const inputSearch = screen.getByTestId('search-input');
+    expect(inputSearch).toBeInTheDocument();
+    userEvent.click(btnSearch);
+    expect(inputSearch).not.toBeInTheDocument();
+  });
+  it('Testa se ao clicar no ícone de perfil é redirecionado para a rota /profile', () => {
+    const { history } = rendeWithRouter(<App />, { initialEntries: ['/done-recipes'] });
+    const { pathname } = history.location;
+    const title = screen.getByRole('heading', { name: /done/i, level: 1 });
     expect(title).toBeInTheDocument();
     expect(pathname).toBe('/done-recipes');
-
-    expect(pathname).toBe('/profile'); */
+    const profileIcon = screen.getByRole('img', { name: /perfil/i });
+    expect(profileIcon).toBeInTheDocument();
+    const btnProfile = screen.getByTestId('profile-top-btn');
+    expect(btnProfile).toBeInTheDocument();
+    userEvent.click(btnProfile);
+    // expect(pathname).toBe('/profile');
   });
 });
