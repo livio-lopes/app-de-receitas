023658385fb3b4412c6f492322bab51b2401f52b
keniapecanha <kenia.silvap@hotmail.com>
2023-05-10 15:40:28 -0300

    Requisito 12 completo

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,17 +1,35 @@
-export const fetchDataIngredient = async (ingredient) => {
+export const fetchMealsIngredient = async (ingredient) => {
   const data = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
   const result = await data.json();
   return result;
 };
 
-export const fetchDataNameSearch = async (name) => {
+export const fetchMealsNameSearch = async (name) => {
   const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`);
   const result = await data.json();
   return result;
 };
 
-export const fetchDataFirstLetter = async (firstLetter) => {
+export const fetchMealsFirstLetter = async (firstLetter) => {
   const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${firstLetter}`);
+  const result = await data.json();
+  return result;
+};
+
+export const fetchDrinksIngredient = async (ingredient) => {
+  const data = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`);
+  const result = await data.json();
+  return result;
+};
+
+export const fetchDrinksNameSearch = async (name) => {
+  const data = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`);
+  const result = await data.json();
+  return result;
+};
+
+export const fetchDrinksFirstLetter = async (firstLetter) => {
+  const data = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f==${firstLetter}`);
   const result = await data.json();
   return result;
 };
