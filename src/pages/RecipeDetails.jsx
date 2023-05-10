c6b6d157cf3f8fe3d1f9c149ba5553545b46e12a
Jefferson Santos <jeff-ds@outlook.com>
2023-05-10 17:53:05 -0300

    .

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -1,46 +1,118 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import ShareAndFavoriteBtns from '../components/ShareAndFavoriteBtns';
 import YoutubeEmbed from '../components/YoutubeEmbed';
-import useFetch from '../hooks/useFetch';
-import useGetIngredients from '../hooks/useGetIngredients';
+import CarouselRecommendations from '../components/CarouselRecommendations';
+import { RecipeDetailsContext } from '../providers/RecipeDetailsProvider';
+
+import './style/fixedButton.css';
 
 export default function RecipeDetails() {
-  const {
-    getIngredients,
-    ingredients,
-    mensures,
-  } = useGetIngredients();
+  const [imageSource, setImageSource] = useState('');
+  const [title, setTitle] = useState('');
+  const [categoryText, setCategoryText] = useState('');
+  const [instructionsText, setInstructionsText] = useState('');
+  const [youtubeVideoID, setYoutubeVideoID] = useState('');
+  const [ingredients, setIngredients] = useState([]);
+  const [mensures, setMensures] = useState([]);
+  const [alcoholic, setAlcoholic] = useState('');
+  const [idFood, setIdFood] = useState();
+  const [nationality, setNationality] = useState('');
 
-  const {
-    imageSource,
-    title,
-    categoryText,
-    instructionsText,
-    youtubeVideoID,
-    alcoholic,
-    fetchData,
-  } = useFetch();
-  // console.log(title);
+  const { setObjectDetails } = useContext(RecipeDetailsContext);
 
   const colectDrinkData = async () => {
-    const idDrink = 178319;
-    const DRINKSURL = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idDrink}`;
-    // Bebidas
-    const data = await fetchData(DRINKSURL);
-    console.log(data);
-    // coletar ingredientes da api de carnes
-    await getIngredients();
+    const idCyPress = 178319;
+    const DRINKSURL = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idCyPress}`;
+    try {
+      // Bebidas
+      const resultsDrinks = await fetch(DRINKSURL);
+      const dataDrinks = await resultsDrinks.json();
+      if (dataDrinks.drinks === null) {
+        console.log('sem bebidas nesse id');
+        return;
+      }
+      if (dataDrinks.drinks !== null) {
+        console.log('bebida nesse id:', dataDrinks.drinks[0]);
+        const {
+          strDrinkThumb,
+          strDrink,
+          strAlcoholic,
+          strInstructions,
+          idDrink,
+        } = dataDrinks.drinks[0];
+        setIdFood(idDrink);
+        setImageSource(strDrinkThumb);
+        setTitle(strDrink);
+        setAlcoholic(strAlcoholic);
+        setInstructionsText(strInstructions);
+
+        // coletar ingredientes da api de carnes
+        const limitAPI = 15;
+        const arrayIngredients = [];
+        const arrayMensures = [];
+        for (let i = 1; i <= limitAPI; i += 1) {
+          const keyIngredients = `strIngredient${i}`;
+          const keyMensures = `strMeasure${i}`;
+          if (dataDrinks.drinks[0][keyIngredients]) {
+            arrayIngredients.push(dataDrinks.drinks[0][keyIngredients]);
+            arrayMensures.push(dataDrinks.drinks[0][keyMensures]);
+          }
+        }
+        setIngredients(arrayIngredients);
+        setMensures(arrayMensures);
+      }
+    } catch (error) {
+      console.log('sem bebidas nesse id', error);
+    }
   };
 
   const colectMealData = async () => {
-    const idMeal = 52771;
-    const MEALURL = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
-    // Comidas
-    const data = await fetchData(MEALURL);
-    console.log(data);
-    // coletar ingredientes da api de carnes
-    await getIngredients();
+    const idCyPress = 52771;
+    const MEALURL = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idCyPress}`;
+    try {
+      // Comidas
+      const resultsFoods = await fetch(MEALURL);
+      const dataFoods = await resultsFoods.json();
+      if (dataFoods.meals === null) {
+        console.log('sem comida nesse id');
+        return;
+      }
+      if (dataFoods.meals !== null) {
+        console.log('comida nesse id:', dataFoods.meals[0]);
+        const { strMealThumb, strMeal, strCategory, strInstructions,
+          strYoutube, idMeal, strArea } = dataFoods.meals[0];
+        setIdFood(idMeal);
+        setNationality(strArea);
+        setImageSource(strMealThumb);
+        setTitle(strMeal);
+        setCategoryText(strCategory);
+        setInstructionsText(strInstructions);
+
+        // id dos videos do youtube
+        const regex = /v=([^&]+)/;
+        const match = strYoutube.match(regex);
+        const videoId = match[1];
+        setYoutubeVideoID(videoId);
+
+        // coletar ingredientes da api de carnes
+        const limitAPI = 20;
+        const arrayIngredients = [];
+        const arrayMensures = [];
+        for (let i = 1; i <= limitAPI; i += 1) {
+          const keyIngredients = `strIngredient${i}`;
+          const keyMensures = `strMeasure${i}`;
+          if (dataFoods.meals[0][keyIngredients]) {
+            arrayIngredients.push(dataFoods.meals[0][keyIngredients]);
+            arrayMensures.push(dataFoods.meals[0][keyMensures]);
+          }
+        }
+        setIngredients(arrayIngredients);
+        setMensures(arrayMensures);
+      }
+    } catch (error) {
+      console.log('caiu no catch', error);
+    }
   };
 
   const setMealObject = useCallback(() => {
