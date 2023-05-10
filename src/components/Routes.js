ee4ff421bd2a61fe7a514676e86b9402d1514aee
keniapecanha <kenia.silvap@hotmail.com>
2023-05-10 17:34:48 -0300

    Requisito 15 faltando somente os testes

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -5,13 +5,12 @@ import Recipes from '../pages/Recipes';
 import Profile from '../pages/Profile';
 import DoneRecipes from '../pages/DoneRecipes';
 import FavoriteRecipes from '../pages/FavoriteRecipes';
-import Meals from '../pages/Meals';
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
-      <Route exact path="/meals" component={ Meals } />
+      <Route exact path="/meals" component={ Recipes } />
       <Route exact path="/drinks" component={ Recipes } />
       <Route exact path="/profile" component={ Profile } />
       <Route exact path="/done-recipes" component={ DoneRecipes } />
