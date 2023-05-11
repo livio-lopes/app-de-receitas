cf81e258743cc0f5e03c0fa480195f9aa5344758
Lívio Lopes <antonio.lvio760@gmail.com>
2023-05-11 17:45:39 -0300

    Tela de receita em progresso
    listando testes

---
+++ src/tests/RecipeInProgress.test.js
@@ -0,0 +1,14 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import rendeWithRouter from './helpers/renderWithRouter';
+import App from '../App';
+
+const MEALS = '/meals/52771/in-progress';
+const DRINKS = '/drinks/178319/in-progress';
+
+describe('Testetando tela de receita em progresso', () => {
+  it.todo('Verifica se um fetch foi realizado');
+  it.todo('Verifica se as informações da receita da comida foi rendenrizada corretamente ');
+  it.todo('Verifica se as informações da receita de bebidas foi rendenrizada corretamente ');
+  it.todo('Verifica se os ingredientes foram redenrizados e se marcar o checkbox o ingrediente e riscado');
+});
