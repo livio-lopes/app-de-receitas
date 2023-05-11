2fa23b9fbb854772e92f2835233af42ff92978eb
Lívio Lopes <antonio.lvio760@gmail.com>
2023-05-11 10:15:41 -0300

    Tele de receita em progresso
    Requisito 37

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -5,6 +5,7 @@ import Recipes from '../pages/Recipes';
 import Profile from '../pages/Profile';
 import DoneRecipes from '../pages/DoneRecipes';
 import FavoriteRecipes from '../pages/FavoriteRecipes';
+import RecipeInProgress from '../pages/RecipeInProgress';
 
 export default function Routes() {
   return (
@@ -12,6 +13,16 @@ export default function Routes() {
       <Route exact path="/" component={ Login } />
       <Route exact path="/meals" component={ Recipes } />
       <Route exact path="/drinks" component={ Recipes } />
+      <Route
+        exact
+        path="/meals/:recipeId/in-progress"
+        component={ RecipeInProgress }
+      />
+      <Route
+        exact
+        path="/drinks/:recipeId/in-progress"
+        component={ RecipeInProgress }
+      />
       <Route exact path="/profile" component={ Profile } />
       <Route exact path="/done-recipes" component={ DoneRecipes } />
       <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
--- src/pages/RecipeInProgress.jsx
+++ src/pages/RecipeInProgress.jsx
@@ -1,7 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
 
 export default function RecipeInProgress() {
+  const [recipe, setRecipe] = useState({});
+  const [isMeals, setIsMeals] = useState(true);
+  const { recipeId } = useParams();
+  const location = useLocation();
+  useEffect(() => {
+    if (location.pathname.includes('meals')) {
+      const URL = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeId}`;
+      fetch(URL)
+        .then((response) => response.json())
+        .then((data) => {
+          setIsMeals(true);
+          setRecipe(data.meals[0]);
+        });
+    }
+    if (location.pathname.includes('drinks')) {
+      const URL = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${recipeId}`;
+      fetch(URL)
+        .then((response) => response.json())
+        .then((data) => {
+          setIsMeals(false);
+          setRecipe(data.drinks[0]);
+        });
+    }
+  }, [setRecipe, recipeId, location.pathname]);
   return (
-    <div>RecipeInProgress</div>
+    <div>
+      <img
+        data-testid="recipe-photo"
+        src={ isMeals ? recipe.strMealThumb : recipe.strDrinkThumb }
+        alt={ isMeals ? recipe.strMeal : recipe.strDrink }
+      />
+
+      <h2 data-testid="recipe-title">{isMeals ? recipe.strMeal : recipe.strDrink}</h2>
+      <button
+        type="button"
+        data-testid="share-btn"
+      >
+        Share
+
+      </button>
+      <button
+        type="button"
+        data-testid="favorite-btn"
+      >
+        Favorite
+
+      </button>
+      <p data-testid="recipe-category">
+        {recipe.strTags}
+      </p>
+      <p data-testid="instructions">
+        {recipe.strInstructions}
+      </p>
+      <button
+        type="button"
+        data-testid="finish-recipe-btn"
+      >
+        Finish Recipe
+
+      </button>
+    </div>
   );
 }
