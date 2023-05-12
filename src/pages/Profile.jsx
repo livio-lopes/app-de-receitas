f969ec832c8ed8f9ed7e51abcc5d36a3ad7194bd
keniapecanha <kenia.silvap@hotmail.com>
2023-05-12 18:42:02 -0300

    Requisito 57

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,9 +2,35 @@ import React from 'react';
 import Header from '../components/Header';
 
 export default function Profile() {
+  const userLogin = () => {
+    if (localStorage.getItem('user')) {
+      const localUser = JSON.parse(localStorage.getItem('user'));
+      return localUser.email;
+    } return '';
+  };
+
   return (
     <div>
       <Header />
+      <h5 data-testid="profile-email">{userLogin()}</h5>
+      <button
+        data-testid="profile-done-btn"
+        type="button"
+      >
+        Done Recipes
+      </button>
+      <button
+        data-testid="profile-favorite-btn"
+        type="button"
+      >
+        Favorite Recipes
+      </button>
+      <button
+        data-testid="profile-logout-btn"
+        type="button"
+      >
+        Logout
+      </button>
     </div>
   );
 }
