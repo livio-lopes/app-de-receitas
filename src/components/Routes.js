757993c6a98f5f8de3ce5ef88982a1af72703ac5
keniapecanha <kenia.silvap@hotmail.com>
2023-05-12 23:24:36 -0300

    Testes do Profile - 100% de cobertura

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -5,29 +5,13 @@ import Recipes from '../pages/Recipes';
 import Profile from '../pages/Profile';
 import DoneRecipes from '../pages/DoneRecipes';
 import FavoriteRecipes from '../pages/FavoriteRecipes';
-import RecipeInProgress from '../pages/RecipeInProgress';
-import RecipeDetails from '../pages/RecipeDetails';
 
 export default function Routes() {
-  const idDaReceitaDrinks = 178319;
-  const idDaReceitaMeals = 52771;
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
       <Route exact path="/meals" component={ Recipes } />
       <Route exact path="/drinks" component={ Recipes } />
-      <Route
-        exact
-        path="/meals/:recipeId/in-progress"
-        component={ RecipeInProgress }
-      />
-      <Route
-        exact
-        path="/drinks/:recipeId/in-progress"
-        component={ RecipeInProgress }
-      />
-      <Route exact path={ `/meals/${idDaReceitaMeals}` } component={ RecipeDetails } />
-      <Route exact path={ `/drinks/${idDaReceitaDrinks}` } component={ RecipeDetails } />
       <Route exact path="/profile" component={ Profile } />
       <Route exact path="/done-recipes" component={ DoneRecipes } />
       <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
