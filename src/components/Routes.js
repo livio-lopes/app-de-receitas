1a8472c65d020bd4e8616e7c390fedb4a9018b4a
keniapecanha <kenia.silvap@hotmail.com>
2023-05-15 10:35:36 -0300

    Tirei o data-testid do botão profile e lupa e coloquei nas imagens

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -5,6 +5,8 @@ import Recipes from '../pages/Recipes';
 import Profile from '../pages/Profile';
 import DoneRecipes from '../pages/DoneRecipes';
 import FavoriteRecipes from '../pages/FavoriteRecipes';
+import RecipeDetails from '../pages/RecipeDetails';
+import RecipeInProgress from '../pages/RecipeInProgress';
 
 export default function Routes() {
   return (
@@ -15,6 +17,19 @@ export default function Routes() {
       <Route exact path="/profile" component={ Profile } />
       <Route exact path="/done-recipes" component={ DoneRecipes } />
       <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
+      <Route exact path="/meals/:id-da-receita" component={ RecipeDetails } />
+      <Route exact path="/drinks/:id-da-receita" component={ RecipeDetails } />
+      <Route
+        exact
+        path="/meals/:id-da-receita/in-progress"
+        component={ RecipeInProgress }
+      />
+      <Route
+        exact
+        path="/drinks/:id-da-receita/in-progress"
+        component={ RecipeInProgress }
+      />
+
     </Switch>
   );
 }
