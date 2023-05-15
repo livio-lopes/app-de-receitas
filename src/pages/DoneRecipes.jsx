800d476679e80db8f8d8a294ab525d48003d1318
AlissonSeraphim <projetoseraphim@gmail.com>
2023-05-15 18:02:17 -0300

    requisito 47 ok

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,16 +1,63 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from '../components/Header';
 import CardDoneRecipes from '../components/CardDoneRecipes';
 import Footer from '../components/Footer';
+import LinkCopiedMessage from '../components/LinkCopiedMessage';
+import { AppContext } from '../providers/AppProvider';
 
 export default function DoneRecipes() {
+  const { statusLinkCopied } = useContext(AppContext);
+  const getLocalStorage = JSON.parse(localStorage.getItem('doneRecipes')) || [];
+  console.log(getLocalStorage);
   return (
-    <div>
+    <div className="d-flex align-items-center justify-content-center vh-100">
       <Header />
       <button data-testid="filter-by-all-btn">All</button>
       <button data-testid="filter-by-meal-btn">Meals</button>
       <button data-testid="filter-by-drink-btn">Drinks</button>
-      <CardDoneRecipes />
+      {
+        statusLinkCopied === true ? (
+          <LinkCopiedMessage />
+        )
+          : null
+      }
+      <div>
+        {
+          getLocalStorage.length > 0 ? (getLocalStorage.map((each, index) => (
+            each.type === 'meal' ? (
+              <CardDoneRecipes
+                key={ each + index }
+                name={ each.name }
+                image={ each.image }
+                nationality={ each.nationality }
+                category={ each.category }
+                doneDate={ each.doneDate }
+                index={ index }
+                tag={ each.tags }
+                type={ each.type }
+                id={ each.id }
+              />
+            ) : (
+              <CardDoneRecipes
+                key={ each + index }
+                name={ each.name }
+                image={ each.image }
+                nationality={ each.nationality }
+                category={ each.category }
+                doneDate={ each.doneDate }
+                index={ index }
+                tag={ each.tags }
+                alcoholicOrNot={ each.alcoholicOrNot }
+                type={ each.type }
+                id={ each.id }
+              />
+            )
+          ))
+          ) : (
+            <h1> LOCAL STORAGE VAZIO MEU PRINCIPE, VOLTE QUANDO TIVER ALGO AQUI </h1>
+          )
+        }
+      </div>
       <Footer />
     </div>
   );
