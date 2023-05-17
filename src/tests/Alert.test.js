12fc7c64e94803d84b63ad9bbb85d62ab7be91a9
keniapecanha <kenia.silvap@hotmail.com>
2023-05-17 10:22:39 -0300

    testes 90% de cobertura do SearchBar

--- src/tests/Alert.test.js
+++ src/tests/Alert.test.js
@@ -7,7 +7,8 @@ import { AppProvider } from '../providers/AppProvider';
 import { RecipeDetailsProvider } from '../providers/RecipeDetailsProvider';
 import App from '../App';
 
-const dataTestIdBtnSearch = 'exec-search-btn';
+const testidbtnSearch = 'exec-search-btn';
+const AlertSorry = 'Sorry, we haven\'t found any recipes for these filters.';
 
 describe('Testando o Alert', () => {
   it('Verifica se ao pesquisar comidas pelo primeira letra, tendo digitado mais de uma, se um alert é disparado', async () => {
@@ -31,7 +32,7 @@ describe('Testando o Alert', () => {
 
     const inputSearch = screen.getByRole('textbox');
     const radioFirstLetter = screen.getByRole('radio', { name: /first letter/i });
-    const btnSearch = screen.getByTestId(dataTestIdBtnSearch);
+    const btnSearch = screen.getByTestId(testidbtnSearch);
 
     userEvent.type(inputSearch, 'lemon');
     userEvent.click(radioFirstLetter);
@@ -41,7 +42,6 @@ describe('Testando o Alert', () => {
       expect(alertSpy).toBeCalled();
     });
 
-    expect(global.alert).toHaveBeenCalledTimes(1);
     expect(alertSpy).toHaveBeenCalledWith('Your search must have only 1 (one) character');
   });
 
@@ -66,7 +66,7 @@ describe('Testando o Alert', () => {
 
     const inputSearch = screen.getByRole('textbox');
     const radioFirstLetter = screen.getByRole('radio', { name: /first letter/i });
-    const btnSearch = screen.getByTestId(dataTestIdBtnSearch);
+    const btnSearch = screen.getByTestId(testidbtnSearch);
 
     userEvent.type(inputSearch, 'lemon');
     userEvent.click(radioFirstLetter);
@@ -76,43 +76,7 @@ describe('Testando o Alert', () => {
       expect(alertSpy).toBeCalled();
     });
 
-    expect(global.alert).toHaveBeenCalledTimes(1);
     expect(alertSpy).toHaveBeenCalledWith('Your search must have only 1 (one) character');
-  });
-
-  it('Verifica se não encontrar uma comida um alert é disparado', async () => {
-    const alertSpy = jest.spyOn(global, 'alert');
-    const { history } = renderWithRouter(
-      <RecipesProvider>
-        <AppProvider>
-          <RecipeDetailsProvider>
-            <App />
-          </RecipeDetailsProvider>
-        </AppProvider>
-      </RecipesProvider>,
-    );
-    act(() => {
-      history.push('/meals');
-    });
-    const { location: { pathname } } = history;
-    expect(pathname).toBe('/meals');
-    const searchIcon = screen.getByRole('img', { name: /search/i });
-    userEvent.click(searchIcon);
-
-    const inputSearch = screen.getByRole('textbox');
-    const radioname = screen.getByRole('radio', { name: /name/i });
-    const btnSearch = screen.getByTestId(dataTestIdBtnSearch);
-
-    userEvent.type(inputSearch, 'comida');
-    userEvent.click(radioname);
-    userEvent.click(btnSearch);
-
-    await waitFor(() => {
-      expect(alertSpy).toBeCalled();
-    });
-
-    expect(global.alert).toHaveBeenCalledTimes(1);
-    expect(alertSpy).toHaveBeenCalledWith('Sorry, we haven\'t found any recipes for these filters.');
   });
 
   it('Verifica se não encontrar nenhuma bebida um alert é disparado', async () => {
@@ -136,7 +100,7 @@ describe('Testando o Alert', () => {
 
     const inputSearch = screen.getByRole('textbox');
     const radioname = screen.getByRole('radio', { name: /name/i });
-    const btnSearch = screen.getByTestId(dataTestIdBtnSearch);
+    const btnSearch = screen.getByTestId(testidbtnSearch);
 
     userEvent.type(inputSearch, 'bebida');
     userEvent.click(radioname);
@@ -146,6 +110,205 @@ describe('Testando o Alert', () => {
       expect(alertSpy).toBeCalled();
     });
 
-    expect(alertSpy).toHaveBeenCalledWith('Sorry, we haven\'t found any recipes for these filters.');
+    expect(alertSpy).toHaveBeenCalledWith(AlertSorry);
+  });
+
+  it('Verifica se não encontrar uma comida, pesquisando pelo ingrediente, um alert é disparado', async () => {
+    const alertSpy = jest.spyOn(global, 'alert');
+    const { history } = renderWithRouter(
+      <RecipesProvider>
+        <AppProvider>
+          <RecipeDetailsProvider>
+            <App />
+          </RecipeDetailsProvider>
+        </AppProvider>
+      </RecipesProvider>,
+    );
+    act(() => {
+      history.push('/meals');
+    });
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/meals');
+    const searchIcon = screen.getByRole('img', { name: /search/i });
+    userEvent.click(searchIcon);
+
+    const inputSearch = screen.getByRole('textbox');
+    const radioIngredient = screen.getByRole('radio', { name: /ingredient/i });
+    const btnSearch = screen.getByTestId(testidbtnSearch);
+
+    userEvent.type(inputSearch, 'jdsksahdfefj');
+    userEvent.click(radioIngredient);
+    userEvent.click(btnSearch);
+
+    await waitFor(() => {
+      expect(alertSpy).toBeCalled();
+    });
+
+    expect(alertSpy).toHaveBeenCalledWith(AlertSorry);
+  });
+
+  it('Verifica se não encontrar uma comida, pesquisando pelo nome, um alert é disparado', async () => {
+    const alertSpy = jest.spyOn(global, 'alert');
+    const { history } = renderWithRouter(
+      <RecipesProvider>
+        <AppProvider>
+          <RecipeDetailsProvider>
+            <App />
+          </RecipeDetailsProvider>
+        </AppProvider>
+      </RecipesProvider>,
+    );
+    act(() => {
+      history.push('/meals');
+    });
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/meals');
+    const searchIcon = screen.getByRole('img', { name: /search/i });
+    userEvent.click(searchIcon);
+
+    const inputSearch = screen.getByRole('textbox');
+    const radioname = screen.getByRole('radio', { name: /name/i });
+    const btnSearch = screen.getByTestId(testidbtnSearch);
+
+    userEvent.type(inputSearch, 'jdsksahdfeffdsfsdfj');
+    userEvent.click(radioname);
+    userEvent.click(btnSearch);
+
+    await waitFor(() => {
+      expect(alertSpy).toBeCalled();
+    });
+
+    expect(alertSpy).toHaveBeenCalledWith(AlertSorry);
+  });
+  // it('Verifica se não encontrar uma bebida, pesquisando pelo ingrediente, um alert é disparado', async () => {
+  //   const alertSpy = jest.spyOn(global, 'alert');
+  //   const { history } = renderWithRouter(
+  //     <RecipesProvider>
+  //       <AppProvider>
+  //         <RecipeDetailsProvider>
+  //           <App />
+  //         </RecipeDetailsProvider>
+  //       </AppProvider>
+  //     </RecipesProvider>,
+  //   );
+  //   act(() => {
+  //     history.push('/drinks');
+  //   });
+  //   const { location: { pathname } } = history;
+  //   expect(pathname).toBe('/drinks');
+  //   const searchIcon = screen.getByRole('img', { name: /search/i });
+  //   userEvent.click(searchIcon);
+
+  //   const inputSearch = screen.getByRole('textbox');
+  //   const radioIngredient = screen.getByRole('radio', { name: /ingredient/i });
+  //   const btnSearch = screen.getByTestId(testidbtnSearch);
+
+  //   userEvent.type(inputSearch, 'jdsksahddfefefefj');
+  //   userEvent.click(radioIngredient);
+  //   userEvent.click(btnSearch);
+
+  //   await waitFor(() => {
+  //     expect(alertSpy).toBeCalled();
+  //   });
+
+  //   expect(alertSpy).toHaveBeenCalledWith(AlertSorry);
+  // });
+
+  it('Verifica se não encontrar uma bebida, pesquisando pelo nome, um alert é disparado', async () => {
+    const alertSpy = jest.spyOn(global, 'alert');
+    const { history } = renderWithRouter(
+      <RecipesProvider>
+        <AppProvider>
+          <RecipeDetailsProvider>
+            <App />
+          </RecipeDetailsProvider>
+        </AppProvider>
+      </RecipesProvider>,
+    );
+    act(() => {
+      history.push('/drinks');
+    });
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/drinks');
+    const searchIcon = screen.getByRole('img', { name: /search/i });
+    userEvent.click(searchIcon);
+
+    const inputSearch = screen.getByRole('textbox');
+    const radioname = screen.getByRole('radio', { name: /name/i });
+    const btnSearch = screen.getByTestId(testidbtnSearch);
+
+    userEvent.type(inputSearch, 'jdsksahdfeffdsfsdfj');
+    userEvent.click(radioname);
+    userEvent.click(btnSearch);
+
+    await waitFor(() => {
+      expect(alertSpy).toBeCalled();
+    });
+
+    expect(alertSpy).toHaveBeenCalledWith(AlertSorry);
+  });
+
+  it('Verifica se pesquisar por uma comida sem escolher um radio, um alert é disparado', async () => {
+    const alertSpy = jest.spyOn(global, 'alert');
+    const { history } = renderWithRouter(
+      <RecipesProvider>
+        <AppProvider>
+          <RecipeDetailsProvider>
+            <App />
+          </RecipeDetailsProvider>
+        </AppProvider>
+      </RecipesProvider>,
+    );
+    act(() => {
+      history.push('/meals');
+    });
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/meals');
+    const searchIcon = screen.getByRole('img', { name: /search/i });
+    userEvent.click(searchIcon);
+
+    const inputSearch = screen.getByRole('textbox');
+    const btnSearch = screen.getByTestId(testidbtnSearch);
+
+    userEvent.type(inputSearch, 'beef');
+    userEvent.click(btnSearch);
+
+    await waitFor(() => {
+      expect(alertSpy).toBeCalled();
+    });
+
+    expect(alertSpy).toHaveBeenCalledWith(AlertSorry);
+  });
+
+  it('Verifica se pesquisar por uma bebida sem escolher um radio, um alert é disparado', async () => {
+    const alertSpy = jest.spyOn(global, 'alert');
+    const { history } = renderWithRouter(
+      <RecipesProvider>
+        <AppProvider>
+          <RecipeDetailsProvider>
+            <App />
+          </RecipeDetailsProvider>
+        </AppProvider>
+      </RecipesProvider>,
+    );
+    act(() => {
+      history.push('/drinks');
+    });
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/drinks');
+    const searchIcon = screen.getByRole('img', { name: /search/i });
+    userEvent.click(searchIcon);
+
+    const inputSearch = screen.getByRole('textbox');
+    const btnSearch = screen.getByTestId(testidbtnSearch);
+
+    userEvent.type(inputSearch, 'milk');
+    userEvent.click(btnSearch);
+
+    await waitFor(() => {
+      expect(alertSpy).toBeCalled();
+    });
+
+    expect(alertSpy).toHaveBeenCalledWith(AlertSorry);
   });
 });
