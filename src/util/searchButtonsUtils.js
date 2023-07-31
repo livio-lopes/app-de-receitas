7f7c7a8968f358d77017a620632d1d95db41df2c
Lívio Lopes <antonio.lvio760@gmail.com>
2023-07-31 19:04:04 -0300

    refactor(iconfilter): change switch and case for listIcons with Array.find

--- src/util/searchButtonsUtils.js
+++ src/util/searchButtonsUtils.js
@@ -3,23 +3,27 @@ import dessert from '../images/dessert.svg';
 import breakfast from '../images/breakfast.svg';
 import goat from '../images/goat.svg';
 import chicken from '../images/chicken.svg';
+import cocoa from '../images/cocoa.svg';
+import drink from '../images/drink.svg';
+import cocktail from '../images/cocktail.svg';
+import shake from '../images/shake.svg';
+import other from '../images/other.svg';
 import all from '../images/All.svg';
 
-const iconFilter = (nameFilter) => {
-  switch (nameFilter) {
-  case 'Beef':
-    return beef;
-  case 'Dessert':
-    return dessert;
-  case 'Breakfast':
-    return breakfast;
-  case 'Goat':
-    return goat;
-  case 'Chicken':
-    return chicken;
-  default:
-    return all;
-  }
-};
+const listIcons = [
+  { name: 'Beef', icon: beef },
+  { name: 'Dessert', icon: dessert },
+  { name: 'Breakfast', icon: breakfast },
+  { name: 'Goat', icon: goat },
+  { name: 'Chicken', icon: chicken },
+  { name: 'All', icon: all },
+  { name: 'Ordinary Drink', icon: drink },
+  { name: 'Cocktail', icon: cocktail },
+  { name: 'Shake', icon: shake },
+  { name: 'Other / Unknown', icon: other },
+  { name: 'Cocoa', icon: cocoa },
+];
+
+const iconFilter = (nameIcon) => listIcons.find((icon) => icon.name === nameIcon).icon;
 
 export default iconFilter;
